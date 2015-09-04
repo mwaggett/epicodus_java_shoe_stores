30f99dc5f599ff83a4731def61d00e23b78f4976
mwaggett <mwaggett@alumni.reed.edu>
2015-09-04 16:41:39 -0700

    Display list of all stores and all brands on homepage with links to add new stores and new brands.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,24 +13,54 @@ public class App {
 
   public static void main(String[] args) {
 
-    staticFileLocation("/public"); // Relative path for images, css, etc.
+    staticFileLocation("/public");
     String layout = "templates/layout.vtl";
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+
+      List<Store> stores = Store.all();
+      List<Brand> brands = Brand.all();
+
+      model.put("stores", stores);
+      model.put("brands", brands);
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    // post("/newpage", (request, response) -> {
-    //   HashMap<String, Object> model = new HashMap<String, Object>();
-    //
-    //   <Class> param = request.queryParams("param");
-    //   model.put("param", param);
-    //
-    //   model.put("template", "templates/newpage.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    get("/stores/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/new-store-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/stores/create", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Store newStore = new Store(request.queryParams("name"));
+      newStore.save();
+
+      response.redirect("/");
+      return null;
+    });
+
+    get("/brands/new", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      model.put("template", "templates/new-brand-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/brands/create", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+
+      Brand newBrand = new Brand(request.queryParams("name"));
+      newBrand.save();
+
+      response.redirect("/");
+      return null;
+    });
 
   }
 
