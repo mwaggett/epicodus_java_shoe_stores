571752ad95de64977980fe0431441a7d253fee0d
mwaggett <mwaggett@alumni.reed.edu>
2015-09-04 13:00:42 -0700

    Add initial files.

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.*;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import java.util.Map;
+
+public class App {
+
+  public static void main(String[] args) {
+
+    staticFileLocation("/public"); // Relative path for images, css, etc.
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    // post("/newpage", (request, response) -> {
+    //   HashMap<String, Object> model = new HashMap<String, Object>();
+    //
+    //   <Class> param = request.queryParams("param");
+    //   model.put("param", param);
+    //
+    //   model.put("template", "templates/newpage.vtl");
+    //   return new ModelAndView(model, layout);
+    // }, new VelocityTemplateEngine());
+
+  }
+
+}
