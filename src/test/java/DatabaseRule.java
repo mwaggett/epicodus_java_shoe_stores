aa5b63e64b712920d75c001ac64f6a6fde0f71db
mwaggett <mwaggett@alumni.reed.edu>
2015-09-04 13:14:17 -0700

    Customize template files.

--- src/test/java/DatabaseRule.java
+++ src/test/java/DatabaseRule.java
@@ -4,15 +4,17 @@ import org.sql2o.*;
 public class DatabaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/database_name_test", null, null);
-    // Make sure to customize the url to have the actual test database name.
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/shoe_stores_test", null, null);
    }
 
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteTableQuery = "DELETE FROM table_name *;"; // Change to actual table name.
-      // v^Add same things for any other tables in the database.^v
-      con.createQuery(deleteTableQuery).executeUpdate();
+      String deleteStoresQuery = "DELETE FROM stores *;";
+      String deleteBrandsQuery = "DELETE FROM brands *;";
+      String deleteStoresBrandsQuery = "DELETE FROM stores_brands *;";
+      con.createQuery(deleteStoresQuery).executeUpdate();
+      con.createQuery(deleteBrandsQuery).executeUpdate();
+      con.createQuery(deleteStoresBrandsQuery).executeUpdate();
     }
   }
 }
